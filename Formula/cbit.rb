264fa7ff483794efc959659003fc6d3bd80f0e6e
0Chord <seaweed.0chord@gmail.com>
2024-09-11 12:48:49 +0900

    update : cbit version to 1.0.6

--- Formula/cbit.rb
+++ Formula/cbit.rb
@@ -1,8 +1,8 @@
 class Cbit < Formula
   desc "cbit 프로젝트입니다."
   homepage ""
-  url "https://github.com/0Chord/cbit/archive/refs/tags/v1.0.5.tar.gz"
-  sha256 "faf4b6b148c010909c30b07c4b5705b612e1761ce1bf77837a35380f6fc997e4"
+  url "https://github.com/0Chord/cbit/archive/refs/tags/v1.0.6.tar.gz"
+  sha256 "d6a356ccbc3e5063f5f7eadb21f273464a6d5f719835e8efaa371291372e0042"
   license "MIT"
 
   depends_on "node"
@@ -13,7 +13,7 @@ class Cbit < Formula
   end
 
   test do
-    assert_match "cbit version 1.0.5", shell_output("#{bin}/cbit --version")
+    assert_match "cbit version 1.0.6", shell_output("#{bin}/cbit --version")
     system "false"
   end
 end
